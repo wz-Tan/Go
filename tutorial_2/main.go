2026179c0d2b945e0db742d74cc4e354059d72d1
wz <wenzhe1423@gmail.com>
2025-12-25 17:53:10 +0800

    error type

--- tutorial_2/main.go
+++ tutorial_2/main.go
@@ -1,19 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	printName("Josh")
 	var num1 int = 2
 	var num2 int = 3
 	var result int = add(num1, num2)
-	fmt.Print("The result of 2 plus 3 is", result)
+	var err = sampleError()
+	if err != nil {
+		fmt.Print("This is an error", err.Error())
+	}
+
+	fmt.Printf("The result of %v plus %v is %vc ", num1, num2, result)
 }
 
 func printName(name string) {
 	fmt.Println("Hello", name)
 }
 
+// Place Return Type after Parentheses, use a bracket for multiple values (int,int)
 func add(num1 int, num2 int) int {
+	var sum int = num1 + num2
+	if sum > 2 {
+		fmt.Print("Sum is greater than 2 ")
+	}
 	return num1 + num2
+}
+
+func sampleError() error {
+	var customErr error
+	customErr = errors.New("This is an error")
+	return customErr
 }
